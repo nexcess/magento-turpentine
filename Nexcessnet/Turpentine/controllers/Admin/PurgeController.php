60ff906d911ac61a5e9820e420e36093e09e457c
aheadley <aheadley@nexcess.net>
2012-06-05 16:55:30 -0400

    remove old-style files

--- Nexcessnet/Turpentine/controllers/Admin/PurgeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Nexcessnet_Turpentine_Admin_PurgeController extends Mage_Adminhtml_Controller_Action {
-    public function indexAction() {
-        $this->loadLayout()
-            ->_addContent($this->getLayout()->createBlock('turpentine/admin_main'))
-            ->renderLayout();
-    }
-}
