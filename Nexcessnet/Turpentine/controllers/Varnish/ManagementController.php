59e9b96e5828a321ccc007278500c2f948c03f9a
aheadley <aheadley@nexcess.net>
2012-08-14 11:22:20 -0400

    add test action

--- Nexcessnet/Turpentine/controllers/Varnish/ManagementController.php
+++ Nexcessnet/Turpentine/controllers/Varnish/ManagementController.php
@@ -19,6 +19,14 @@ class Nexcessnet_Turpentine_Varnish_ManagementController
             ->renderLayout();
     }
 
+    public function testAction() {
+        $socket = Mage::getModel( 'turpentine/varnish_admin_socket' );
+        $socket->setHost( '127.0.0.1' );
+        $socket->setPort( 6082 );
+        var_dump( $socket->vcl_list() );
+        exit();
+    }
+
     /**
      * Full flush action, flushes all Magento URLs in Varnish cache
      *
