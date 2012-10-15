30b711294fd5b1fcdced3b591df6f0e3224c5d57
aheadley <aheadley@nexcess.net>
2012-10-15 18:23:20 -0400

    stub out some esi stuff

---
+++ app/code/community/Nexcessnet/Turpentine/controllers/Esi.php
@@ -0,0 +1,11 @@
+<?php
+
+class Nexcessnet_Turpentine_EsiController extends Mage_Core_Controller_Front_Action {
+    public function indexAction() {
+        return $this->getBlockAction();
+    }
+
+    public function getBlockAction() {
+
+    }
+}
