6ad353e679e8fc24ec6eed9eaf773b62b0e4388d
aheadley <aheadley@nexcess.net>
2012-12-04 19:18:11 -0500

    Revert "maybe fix duplicate footer"
    This reverts commit a9d3ae42a4c0fa6d03cb9bb0ea7eaebf1b857f08.

--- app/code/community/Nexcessnet/Turpentine/controllers/EsiController.php
+++ app/code/community/Nexcessnet/Turpentine/controllers/EsiController.php
@@ -96,11 +96,9 @@ class Nexcessnet_Turpentine_EsiController extends Mage_Core_Controller_Front_Act
         Mage::getModel( 'turpentine/shim_mage_core_layout' )
             ->generateFullBlock( $blockNode );
         foreach( $nodesToGenerate as $nodeName ) {
-            if( $nodeName != (string)$blockNode['name'] ) {
-                foreach( $layout->getNode()->xpath( sprintf(
-                        '//reference[@name=\'%s\']', $nodeName ) ) as $node ) {
-                    $layout->generateBlocks( $node );
-                }
+            foreach( $layout->getNode()->xpath( sprintf(
+                    '//reference[@name=\'%s\']', $nodeName ) ) as $node ) {
+                $layout->generateBlocks( $node );
             }
         }
         return $layout->getBlock( $esiData->getNameInLayout() );
