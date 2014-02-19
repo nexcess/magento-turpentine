d747e9ce32d45c43bb13bb411ddffdabb937185c
Adam Hall <adam.hall.evensis@gmail.com>
2014-02-19 12:28:52 +0000

    fix typo
    Fixed typo in module name check.

--- app/code/community/Nexcessnet/Turpentine/Helper/Cron.php
+++ app/code/community/Nexcessnet/Turpentine/Helper/Cron.php
@@ -171,7 +171,7 @@ class Nexcessnet_Turpentine_Helper_Cron extends Mage_Core_Helper_Abstract {
                     $urls[] = $prod->getProductUrl();
                 }
             }
-            $sitemap = (Mage::getConfig()->getNode('modules/Mageworx_XSitemap') !== FALSE) ?
+            $sitemap = (Mage::getConfig()->getNode('modules/MageWorx_XSitemap') !== FALSE) ?
                                                            'xsitemap/cms_page' : 'sitemap/cms_page';
             foreach( Mage::getResourceModel( $sitemap )
                         ->getCollection( $storeId ) as $item ) {
