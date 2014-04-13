ee83a9f1505d7aab335956ce505d0e39cb7a1a2d
Jan-Simon Winkelmann <eth@limodo.de>
2014-04-13 15:58:21 +0200

    Support varnish4 response structure when parsing cli_buffer param

--- app/code/community/Nexcessnet/Turpentine/Model/Varnish/Admin/Socket.php
+++ app/code/community/Nexcessnet/Turpentine/Model/Varnish/Admin/Socket.php
@@ -391,9 +391,17 @@ class Nexcessnet_Turpentine_Model_Varnish_Admin_Socket {
         $dataLength = strlen( $data );
         if( $dataLength >= self::CLI_CMD_LENGTH_LIMIT ) {
             $cliBufferResponse = $this->param_show( 'cli_buffer' );
-            if( preg_match( '~^cli_buffer\s+(\d+)\s+\[bytes\]~',
-                    $cliBufferResponse['text'], $match ) ) {
+            $regexp = '~^cli_buffer\s+(\d+)\s+\[bytes\]~';
+            if ( $this->getVersion()==='4.0' ) {
+                // Varnish4 supports "16k" style notation
+                $regexp = '~^cli_buffer\s+Value is:\s+(\d+)([k|m|g]{1})?\s+\[bytes\]~';
+            }
+            if( preg_match( $regexp, $cliBufferResponse['text'], $match ) ) {
                 $realLimit = (int)$match[1];
+                if ( isset($match[2]) ) {
+                    $factors = array('k'=>1,'m'=>2,'g'=>3);
+                    $realLimit *= pow(1024, $factors[$match[2]]);
+                }
             } else {
                 Mage::helper( 'turpentine/debug' )->logWarn(
                     'Failed to determine Varnish cli_buffer limit, using default' );
