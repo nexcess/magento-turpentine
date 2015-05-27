9ab8756410a874e3ed3fe277784a6bbd5c0dc491
aricwatson <awatson@Aric-Watsons-MacBook-Pro.local>
2015-05-27 13:55:21 -0400

    Fix for community poll
    Changes method to ajax, and rewrites ActivePoll to display the ajax
    template.

---
+++ app/code/community/Nexcessnet/Turpentine/Block/Poll/ActivePoll.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Nexcess.net Turpentine Extension for Magento
+ * Copyright (C) 2012  Nexcess.net L.L.C.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+class Nexcessnet_Turpentine_Block_Poll_Activepoll extends Mage_Poll_Block_ActivePoll {
+
+    public function setTemplate($template)
+    {
+    	$debugHelper = Mage::helper( 'turpentine/debug' );
+    	$debugHelper->logInfo('Set poll template');
+
+        $this->_template = $template;
+        $this->setPollTemplate('turpentine/ajax.phtml', 'poll' );
+        $this->setPollTemplate('turpentine/ajax.phtml', 'results' );         
+        return $this;
+    }
+}
