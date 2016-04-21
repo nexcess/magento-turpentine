d3eef2575f9c7ed9ec9e416b4167887080f5f9f6
Craig Carnell <craig_carnell@hotmail.com>
2016-04-21 15:32:31 +0100

    fix broken cms page override for theme

--- app/code/community/Nexcessnet/Turpentine/Model/Observer/Esi.php
+++ app/code/community/Nexcessnet/Turpentine/Model/Observer/Esi.php
@@ -325,6 +325,8 @@ class Nexcessnet_Turpentine_Model_Observer_Esi extends Varien_Event_Observer {
         $methodParam = $esiHelper->getEsiMethodParam();
         $esiData = new Varien_Object();
         $esiData->setStoreId(Mage::app()->getStore()->getId());
+        $esiData->setDesignPackage( Mage::getDesign()->getPackageName() );
+        $esiData->setDesignTheme( Mage::getDesign()->getTheme( 'layout' ) );
         $esiData->setNameInLayout($blockObject->getNameInLayout());
         $esiData->setBlockType(get_class($blockObject));
         $esiData->setLayoutHandles($this->_getBlockLayoutHandles($blockObject));
