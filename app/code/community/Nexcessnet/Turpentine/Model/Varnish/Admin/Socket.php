3582803b399b72c45eb34007fe7f7f17eecf4794
Dmytro Vasylenko <dmytro.vasylenko@eltrino.com>
2016-10-01 14:39:08 +0300

    Fix possible issue with limit validation

--- app/code/community/Nexcessnet/Turpentine/Model/Varnish/Admin/Socket.php
+++ app/code/community/Nexcessnet/Turpentine/Model/Varnish/Admin/Socket.php
@@ -412,7 +412,7 @@ class Nexcessnet_Turpentine_Model_Varnish_Admin_Socket {
             if (preg_match($regexp, $cliBufferResponse['text'], $match)) {
                 $realLimit = (int) $match[1];
                 if (isset($match[2])) {
-                    $factors = array('k'=>1, 'm'=>2, 'g'=>3);
+                    $factors = array('b'=>0, 'k'=>1, 'm'=>2, 'g'=>3);
                     $realLimit *= pow(1024, $factors[$match[2]]);
                 }
             } else {
