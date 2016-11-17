0d5287849a87412399b9066e1eecb856e9e12ff1
Phu Hoang <hmphu.it@gmail.com>
2016-11-17 20:49:10 +0700

    Migrate vcl_synth subroutine for Varnish v4

--- app/code/community/Nexcessnet/Turpentine/Model/Varnish/Configurator/Abstract.php
+++ app/code/community/Nexcessnet/Turpentine/Model/Varnish/Configurator/Abstract.php
@@ -969,6 +969,40 @@ EOS;
             'vcl_synth_content' => Mage::getStoreConfig('turpentine_vcl/maintenance/custom_vcl_synth')));
     }
 
+    /**
+     * vcl_synth for fixing https
+     *
+     * @return string
+     */
+    protected function _vcl_sub_synth_https_fix()
+    {
+        $tpl = $this->_vcl_sub_synth();
+
+        if(!$tpl){
+            $tpl = <<<EOS
+sub vcl_synth {
+    if (resp.status == 750) {
+        set resp.status = 301;
+        set resp.http.Location = "https://" + req.http.host + req.url;
+        return(deliver);
+    }
+}
+EOS;
+        }else{
+            $tpl_750 = '
+sub vcl_synth {
+    if (resp.status == 750) {
+        set resp.status = 301;
+        set resp.http.Location = "https://" + req.http.host + req.url;
+        return(deliver);
+    }';
+
+        $tpl = str_ireplace('sub vcl_synth {', $tpl_750, $tpl);
+        }
+
+        return $tpl;
+    }
+
 
 
     /**
@@ -1051,6 +1085,9 @@ EOS;
         
         if (Mage::getStoreConfig('turpentine_varnish/general/https_redirect_fix')) {
             $vars['https_redirect'] = $this->_vcl_sub_https_redirect_fix();
+            if(Mage::getStoreConfig('turpentine_varnish/servers/version') == '4.0'){
+                $vars['vcl_synth'] = $this->_vcl_sub_synth_https_fix();
+            }
         }
 
         foreach (array('','top') as $position) {
